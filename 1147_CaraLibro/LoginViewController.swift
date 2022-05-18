bbca1f964ae44b7c73b42a36c193d422a3589723
Carlos Quispe <carlosquispe1805@gmail.com>
2022-05-18 10:02:50 -0500

    Parte de Anthony y Sergio

--- 1147_CaraLibro/LoginViewController.swift
+++ 1147_CaraLibro/LoginViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    @IBAction private func tapTopCloseKeyboard(sender: UITapGestureRecognizer){
+        self.view.endEditing(true)
+    }
     	
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +20,7 @@ class LoginViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("LoginViewController - viewWillAppear")
+        self.registerKeyboardNotifications()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -26,7 +30,12 @@ class LoginViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        print("LoginViewController - viewWillDisappear")
+        self.unregisterKeyboardNotifications()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("LoginViewController - viewDidDisappear")
     }
     
     private func registerKeyboardNotifications()
@@ -36,18 +45,18 @@ class LoginViewController: UIViewController {
 
     }
     
-    private func unregisterKeyboardNotifications ()
-    {
-        
-    }
-    @objc private func keyboardWillShow(_ notification: Notification)
-    {
+    private func unregisterKeyboardNotifications (){
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
         
     }
     
-    @objc private func keyboardWillhide(_ notification: Notification)
-    {
-        
+    @objc private func keyboardWillShow(_ notification: Notification){
+        print("TECLADO APARECE")
+    }
+    
+    @objc private func keyboardWillhide(_ notification: Notification){
+        print("TECLADO DESAPARECE")
     }
     
 }
--- 1147_CaraLibro/OlvidarContraViewController.swift
+++ 1147_CaraLibro/OlvidarContraViewController.swift
@@ -9,4 +9,54 @@ import UIKit
 
 class OlvidarContraViewController: UIViewController {
     
+    @IBAction private func tapTopCloseKeyboard(sender: UITapGestureRecognizer){
+        self.view.endEditing(true)
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("LoginViewController - viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.registerKeyboardNotifications()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("LoginViewController - viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.unregisterKeyboardNotifications()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("LoginViewController - viewDidDisappear")
+    }
+    
+    private func registerKeyboardNotifications()
+    {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillhide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+    }
+    
+    private func unregisterKeyboardNotifications (){
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification){
+        print("TECLADO APARECE")
+    }
+    
+    @objc private func keyboardWillhide(_ notification: Notification){
+        print("TECLADO DESAPARECE")
+    }
+    
 }
